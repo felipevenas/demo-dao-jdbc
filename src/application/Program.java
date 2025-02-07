6901108935c02af7555dd5a9fe69829ec921ef57
Felipe Venas <dev.felipevenas@gmail.com>
2025-02-06 23:53:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-	Department obj = new Department(1, "Books");
-	System.out.println(obj);
-		
+		Seller seller = new Seller(23, "Felipe", "felipe@gmail.com", new Date(), 3000.00, new Department(1, "Books"));
+		System.out.println(seller);
 	}
 
 }
