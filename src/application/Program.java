82a530e7242e3c68b7a11d90a95eb1849709e7d7
Felipe Venas <dev.felipevenas@gmail.com>
2025-02-07 00:05:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Seller seller = new Seller(23, "Felipe", "felipe@gmail.com", new Date(), 3000.00, new Department(1, "Books"));
-		System.out.println(seller);
+		
+		
 	}
-
 }
