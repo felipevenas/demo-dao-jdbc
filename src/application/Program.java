1ad4419a9f99375ed62ad09092badbb374ce7350
Felipe Venas <dev.felipevenas@gmail.com>
2025-02-07 00:25:45 -0300

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,23 @@
 package application;
 
-import java.util.Date;
-
-import entities.Department;
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		
+		/*
+		 * Não precisamos instanciar diretamente no código, 
+		 * apenas é preciso chamar o método de criação em DaoFactory.
+		 */
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 		
 		
 	}
--- src/dao/impl/SellerDaoJDBC.java
+++ src/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import dao.SellerDao;
+import db.DB;
+import db.DbException;
+import entities.Department;
 import entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,43 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			st.setInt(1, id); // --> Passando o id declarado no escopo do método.
+			rs = st.executeQuery();
+			if (rs.next()) { 
+				Department dep = new Department();
+				// --> Criou a referência do objeto do tipo Department para acessar seus métodos e atulizar seus atributos.
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				// --> Criou a referência do objeto do tipo Seller para acessar seus métodos e atualizar seus atributos.
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
