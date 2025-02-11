d618b20a03f936ff10f9e6111e30ad23bad28e3d
Felipe Venas <dev.felipevenas@gmail.com>
2025-02-10 23:33:20 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import dao.DaoFactory;
@@ -39,6 +40,11 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println();
 		
+		System.out.println("=== Quarter Test: Seller insert ======");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
